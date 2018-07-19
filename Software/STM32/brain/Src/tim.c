05538e18188851106eddb6ee090257bd86867aff
Ro-ee Tal <roeetal1@gmail.com>
2018-07-18 22:28:50 -0700

    servo basket integration

--- Software/STM32/brain/Src/tim.c
+++ Software/STM32/brain/Src/tim.c
@@ -170,9 +170,9 @@ void MX_TIM3_Init(void)
   TIM_OC_InitTypeDef sConfigOC;
 
   htim3.Instance = TIM3;
-  htim3.Init.Prescaler = 0;
+  htim3.Init.Prescaler = 720;
   htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim3.Init.Period = 1000;
+  htim3.Init.Period = 2000;
   htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
   {
